a2dc0218bcc04e5fe6c35725591b74ad3b499aae
Dishadrach <adamsshadrachdickson@gmail.com>
2023-01-09 00:22:13 -0800

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+A class that inherits from class 'list'
+"""
+
+def print_sorted(self):
+    """print_sorted - method to sort a list"""
+    l = (sorted(self[:]))
+    print(l)
